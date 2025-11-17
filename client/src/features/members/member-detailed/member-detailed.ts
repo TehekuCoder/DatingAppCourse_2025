37377dd9238e1be99369176ab9e5c1deeb819132
TehekuCoder <j.d.w.w@gmx.de>
2025-11-17 19:20:07 +0100

    End of section 10

--- client/src/features/members/member-detailed/member-detailed.ts
+++ client/src/features/members/member-detailed/member-detailed.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, Signal, signal } from '@angular/core';
+import { Component, computed, inject, OnInit, Signal, signal } from '@angular/core';
 import {
   ActivatedRoute,
   NavigationEnd,
@@ -10,23 +10,27 @@ import {
 import { filter, Observable } from 'rxjs';
 import { Member } from '../../../types/member';
 import { AgePipe } from '../../../core/pipes/age-pipe';
+import { AccountService } from '../../../core/services/account-service';
+import { MemberService } from '../../../core/services/member-service';
 
 @Component({
   selector: 'app-member-detailed',
-  imports: [ RouterLink, RouterLinkActive, RouterOutlet, AgePipe],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, AgePipe],
   templateUrl: './member-detailed.html',
   styleUrl: './member-detailed.css',
 })
 export class MemberDetailed implements OnInit {
   private route = inject(ActivatedRoute);
+  protected memberService = inject(MemberService);
+  private accountService = inject(AccountService);
   private router = inject(Router);
-  protected member = signal<Member | undefined>(undefined);
   protected title = signal<string | undefined>('Profile');
+  protected isCurrentUser = computed(() => {
+    return this.accountService.currentUser()?.id === this.route.snapshot.paramMap.get('id');
+  });
 
   ngOnInit(): void {
-    this.route.data.subscribe({
-      next: (data) => this.member.set(data['member']),
-    });
+
     this.title.set(this.route.firstChild?.snapshot?.title);
 
     this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe({
@@ -35,5 +39,4 @@ export class MemberDetailed implements OnInit {
       },
     });
   }
-
 }
