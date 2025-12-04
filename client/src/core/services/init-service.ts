07dc96f054df6eb58f6881ba5b0662519e2a6027
TehekuCoder <j.d.w.w@gmx.de>
2025-12-04 22:51:46 +0100

    End of section 16

--- client/src/core/services/init-service.ts
+++ client/src/core/services/init-service.ts
@@ -1,21 +1,22 @@
 import { inject, Injectable } from '@angular/core';
 import { AccountService } from './account-service';
-import { Observable, of } from 'rxjs';
-import { LikesServices } from './likes-services';
+import { tap } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class InitService {
   private accountService = inject(AccountService);
-private likesService = inject(LikesServices);
-  init(){
-    const userString = localStorage.getItem('user');
-    if (!userString) return of(null);
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-    this.likesService.getLikeIds();
 
-    return of(null)
+  init() {
+    return this.accountService.refreshToken().pipe(
+      tap((user) => {
+        if (user) {
+          this.accountService.setCurrentUser(user);
+          this.accountService.startTokenRefreshInterval();
+        }
+      })
+    );
   }
 }
